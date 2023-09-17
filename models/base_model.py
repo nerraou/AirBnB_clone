cdbf0ae61a22e86a587d6b3122f2e25fef5f9efb
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-09-17 18:56:05 +0100

    fix(BaseModel): __str__

--- models/base_model.py
+++ models/base_model.py
@@ -33,7 +33,7 @@ class BaseModel():
 
     def __str__(self):
         """return: [<class name>] (<self.id>) <self.__dict__>"""
-        return "[{}] ({}) {}".format(BaseModel.__name__,
+        return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
 
     def save(self):
