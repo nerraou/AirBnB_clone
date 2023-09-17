346ef99c8478fb643ad2d7465f43c32eff653d68
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-09-17 17:54:37 +0100

    feat: add parse function to console

--- console.py
+++ console.py
@@ -3,15 +3,17 @@
 
 
 import cmd
-from models.base_model import BaseModel
 from models import storage
+import re
+from models.base_model import BaseModel
+from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
     """This is Hbnb class; console managment"""
     prompt = '(hbnb) '
 
-    __classes = {"BaseModel": BaseModel}
+    __classes = {"BaseModel": BaseModel, "User": User}
 
     def emptyline(self):
         """Do nothing"""
@@ -66,6 +68,100 @@ class HBNBCommand(cmd.Cmd):
             return False
 
         print(objects[key])
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        args = arg.split(" ")
+
+        if len(args[0]) == 0:
+            print("** class name missing **")
+            return False
+
+        if args[0] not in self.__classes:
+            print("** class doesn't exist **")
+            return False
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return False
+        objects = storage.all()
+        key = "{}.{}".format(args[0], args[1])
+
+        if key not in objects:
+            print("** no instance found **")
+            return False
+        objects.pop(key)
+        storage.save()
+
+    def do_all(self, arg):
+        """Prints all string representation of
+          all instances based or not on the class name."""
+        objects = storage.all()
+        if len(arg) == 0:
+            array = []
+            for key in objects:
+                array.append(str(objects[key]))
+            print(array)
+        else:
+            if arg not in self.__classes:
+                print("** class doesn't exist **")
+                return False
+            array = []
+            for key in objects:
+                obj = objects[key]
+                if obj.__class__.__name__ == arg:
+                    array.append(str(obj))
+            print(array)
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id
+            by adding or updating attribute"""
+        args = parse(arg)
+
+        if len(args[0]) == 0:
+            print("** class name missing **")
+            return False
+
+        if args[0] not in self.__classes:
+            print("** class doesn't exist **")
+            return False
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return False
+
+        objects = storage.all()
+        key = "{}.{}".format(args[0], args[1])
+
+        if key not in objects:
+            print("** no instance found **")
+            return False
+
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return False
+
+        if len(args) < 4:
+            print("** value missing **")
+            return False
+        attribute = args[2]
+        value = args[3]
+        obj = objects[key]
+        currentValue = getattr(obj, attribute)
+        obj_type = type(currentValue)
+        setattr(obj, attribute, obj_type(value))
+        storage.save()
+
+
+def parse(arg):
+    """parse command line argument"""
+    args = arg.split(" ")
+
+    if len(args) >= 4 and args[3][0] == '"':
+        start_index = arg.find('"')
+        end_index = arg.find('"', start_index + 1)
+        args[3] = arg[start_index + 1:end_index]
+    return args
 
 
 if __name__ == '__main__':
